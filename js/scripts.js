849544597eef3a7c1f1787a245ec926a801c1347
Zachary Baum <zacharybaum42@gmail.com>
2016-08-19 10:47:29 -0400

    add visible counter to user interface

--- js/scripts.js
+++ js/scripts.js
@@ -4,25 +4,33 @@ $("#form").submit(function(event) {
 
   var input = parseInt($("input#numberInput").val());
   console.log(input)
-  var numbers = []
+  var numbers = [];
 
-  var counter = function(targetNum){
-    for (i=1; i <= targetNum; i++) {
-      if ((i % 15) === 0) {
-        $("#output").append("Ping Pong! ")
-      }
-      else if ((i % 3) === 0) {
-        $("#output").append("ping, ");
-      }
-      else if ((i % 5) === 0) {
-        $("#output").append("pong, ")
-      }
-      else {
-        $("#output").append(i + ", ");
-      }
+  for (i=1; i <= input; i++) {
+    if ((i % 15) === 0) {
+      numbers.push("Ping Pong");
+    }
+    else if ((i % 3) === 0) {
+      numbers.push("ping");
+    }
+    else if ((i % 5) === 0) {
+      numbers.push("pong")
+    }
+    else {
+      numbers.push(i);
     }
   }
+  console.log(numbers);
 
-  setInterval(counter(input), 10000)
+  var i = 0
+
+  var printing = setInterval(function() {
+    $("#bigNumber").text(numbers[i])
+    $("#output").append(numbers[i] + ", ")
+    i++
+    if (i >= numbers.length) {
+      clearInterval(printing)
+    }
+  }, (5000 / numbers.length))
 
 });
