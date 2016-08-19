f673911e50a3695b30e6dd5234efcf14c80118e8
Zachary Baum <zacharybaum42@gmail.com>
2016-08-19 11:07:02 -0400

    add ping pong totals to interface

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,22 @@ $("#form").submit(function(event) {
   var input = parseInt($("input#numberInput").val());
   console.log(input)
   var numbers = [];
+  pingPongs = 0;
+  pings = 0;
+  pongs = 0;
 
   for (i=1; i <= input; i++) {
     if ((i % 15) === 0) {
       numbers.push("Ping Pong");
+      pingPongs++;
     }
     else if ((i % 3) === 0) {
       numbers.push("ping");
+      pings++;
     }
     else if ((i % 5) === 0) {
       numbers.push("pong")
+      pongs++;
     }
     else {
       numbers.push(i);
@@ -25,12 +31,17 @@ $("#form").submit(function(event) {
   var i = 0
 
   var printing = setInterval(function() {
-    $("#bigNumber").text(numbers[i])
+    $("#bigNumber").text(i)
     $("#output").append(numbers[i] + ", ")
     i++
     if (i >= numbers.length) {
+      $("#bigNumber").text(i++)
       clearInterval(printing)
     }
   }, (5000 / numbers.length))
+console.log(pings)
+  $(".pings").text(pings);
+  $(".pongs").text(pongs);
+  $(".pingPongs").text(pingPongs);
 
 });
