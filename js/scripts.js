aa4952f75d3f0f87feadb47dae7b6573d31d6bd6
zacharybaum42@gmail.com <zacharybaum42@gmail.com@>
2016-12-22 14:10:24 -0500

    make minor fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,57 @@
-$("#form").submit(function(event) {
-  event.preventDefault();
+function pingPong(input) {
   $("#output").empty();
-
-  var input = parseInt($("input#numberInput").val());
-  console.log(input)
-  var numbers = [];
+  var output = [];
   pingPongs = 0;
   pings = 0;
   pongs = 0;
 
-  for (i=1; i <= input; i++) {
-    if ((i % 15) === 0) {
-      numbers.push("<b><i>Ping Pong</i></b>");
-      pingPongs++;
-      pings++;
-      pongs++;
+  if (input <= 0) {
+    alert("You must enter a positive, whole number.");
+    $('#numberInput').val("");
+  } else {
+    for (i=1; i <= input; i++) {
+      if ((i % 15) === 0) {
+        output.push("<b><i>Ping Pong</i></b>");
+        pingPongs++;
+        pings++;
+        pongs++;
+      }
+      else if ((i % 3) === 0) {
+        output.push("<b>ping</b>");
+        pings++;
+      }
+      else if ((i % 5) === 0) {
+        output.push("<b>pong</b>")
+        pongs++;
+      }
+      else {
+        output.push(i);
+      }
     }
-    else if ((i % 3) === 0) {
-      numbers.push("<b>ping</b>");
-      pings++;
-    }
-    else if ((i % 5) === 0) {
-      numbers.push("<b>pong</b>")
-      pongs++;
-    }
-    else {
-      numbers.push(i);
-    }
+    console.log(output);
+
+    var i = 0
+
+    var printing = setInterval(function() {
+      $("#bigNumber").text(i)
+      $("#output").append(output[i] + ", ")
+      i++
+      if (i >= output.length) {
+        $("#bigNumber").text(i++)
+        $(".pings").text(pings);
+        $(".pongs").text(pongs);
+        $(".pingPongs").text(pingPongs);
+        clearInterval(printing)
+      }
+    }, (3000 / output.length))
   }
-  console.log(numbers);
+}
 
-  var i = 0
+$("#form").submit(function(event) {
+  event.preventDefault();
 
-  var printing = setInterval(function() {
-    $("#bigNumber").text(i)
-    $("#output").append(numbers[i] + ", ")
-    i++
-    if (i >= numbers.length) {
-      $("#bigNumber").text(i++)
-      $(".pings").text(pings);
-      $(".pongs").text(pongs);
-      $(".pingPongs").text(pingPongs);
-      clearInterval(printing)
-    }
-  }, (5000 / numbers.length))
-console.log(pings)
+  var input = parseInt($("input#numberInput").val());
 
+  pingPong(input);
 
 });
